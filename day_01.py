26b1bd520d06165973baa1fc032b753aa91aa3b7
Luke <luke@winkreports.com>
2023-01-19 09:45:05 +1100

    Complete day 1 challenge.

---
+++ day_01.py
@@ -0,0 +1,33 @@
+INPUT = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+def parse_by_double_linebreaks(str):
+        return str.split("\n\n")
+
+def parse_by_single_linebreaks(str):
+    return list(map(int, str.split()))
+
+def parse_elves(str):
+    # Take a multi-line input and
+    return [parse_by_single_linebreaks(elf) for elf in parse_by_double_linebreaks(str)]
+
+def subtotal_elf(elf):
+    return sum(elf)
+
+if __name__ == '__main__':
+    elves = parse_elves(INPUT)
+    subtotals = [subtotal_elf(elf) for elf in elves]
+    highest = max(subtotals)
+    print("The elf with the most calories is carrying " + str(highest) + " calories.")
